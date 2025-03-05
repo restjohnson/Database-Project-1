c4501aa35f38e592e0938055011eb08787a970b4
mwalla55 <124012816+mwalla55@users.noreply.github.com>
2025-03-05 12:10:36 -0500

    creates billing info and inventory tables

---
+++ billingInfoAndInventoryCreator.sql
@@ -0,0 +1,19 @@
+USE EMR_DB;
+
+CREATE TABLE IF NOT EXISTS billingInfo (
+	bill_ID int AUTO_INCREMENT PRIMARY KEY,
+    first_name varchar(255),
+    last_name varchar(255),
+    amount int,
+    paymentStatus varchar(255),
+    patient_id int,
+    appointment_ID int,
+    FOREIGN KEY (patient_id) REFERENCES demographics(patient_id),
+    FOREIGN KEY (appointment_ID) REFERENCES appointment(appointment_ID)
+);
+
+CREATE TABLE IF NOT EXISTS inventory (
+	item_id int AUTO_INCREMENT PRIMARY KEY,
+    itemName varchar(255),
+    quantityAvailable int
+)
