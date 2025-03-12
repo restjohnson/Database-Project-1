f10cf002af99db62de428c5ef89189d7b0c7d0e3
Rest Johnson <149099469+restjohnson@users.noreply.github.com>
2025-03-12 01:54:08 -0400

    Updated tables with test data

--- tablesPatientRecord&Appointment.sql
+++ tablesPatientRecord&Appointment.sql
@@ -20,3 +20,32 @@ CREATE TABLE IF NOT EXISTS appointment (
     FOREIGN KEY (dentist_id) REFERENCES dentist(dentist_id) ON DELETE CASCADE,
     FOREIGN KEY (record_id) REFERENCES patientRecord(record_id) ON DELETE SET NULL
 );
+
+-- made an altercation to the treatement column
+/*ALTER TABLE patientRecord
+RENAME COLUMN treatment TO treatment_proposed; 
+*/
+
+-- records for the patientRecord and appointment tables
+INSERT INTO patientRecord (diagnosis, treatment_proposed, patient_ID, dentist_ID) VALUES 
+('Tooth Cavity', 'Fillings and Crowns', 1, 1),
+('Gum Infection', 'Antibiotics', 2, 3),
+('Tooth Extraction', 'Surgical removal of wisdom tooth', 3, 4),
+('Dental Misalignment', 'Teeth Alignement Performed', 5, 2),
+('Dental Discoloration', 'Teeth Whitening', 4, 1),
+('Braces Adjustment', 'Tightened braces', 5, 2),
+('Cavity', 'Dental filling only', 4, 5);
+
+INSERT INTO appointment(appointmentDate, location, appointment_status, patient_id, dentist_id, record_id)
+VALUES 
+('2025-03-09 10:00:00', '123 Dental Ave, Charlotte, NC', 'Completed', 1, 1, 1),
+('2025-03-09 14:00:00', '789 Tooth Rd, Charlotte, NC', 'Completed', 2, 3, 2),
+('2025-03-12 09:30:00', '321 Gum Ln, Charlotte, NC', 'Completed', 3, 4, 3),
+('2025-03-18 11:15:00', '456 Smile St, Charlotte, NC', 'Scheduled', 5, 2, 4),
+('2025-03-19 13:00:00', '123 Dental Ave, Charlotte, NC', 'Scheduled', 4, 1, 5),
+('2025-03-19 13:00:00', '456 Smile St, Charlotte, NC', 'Scheduled', 5, 2, 6),
+('2025-03-21 13:00:00', '654 Cavity Blvd, Charlotte, NC', 'Scheduled', 4, 5, 7);
+
+
+SELECT * FROM patientRecord;
+SELECT * from appointment;
