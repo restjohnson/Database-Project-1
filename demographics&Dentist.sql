f47e81f0f3948651504898682c6cf2933a3802e4
NicKanampiu <78459256+NicKanampiu@users.noreply.github.com>
2025-03-12 12:17:55 -0400

    Update demographics&Dentist.sql to add Insurance numbers

--- demographics&Dentist.sql
+++ demographics&Dentist.sql
@@ -7,6 +7,7 @@ CREATE TABLE demographics (
     address VARCHAR(100) NOT NULL,              -- Address
     phone VARCHAR(15) NOT NULL,                 -- Phone number
     email VARCHAR(100)                          -- Email
+    insurance_number VARCHAR(100) NOT NULL      -- Insurance number
 );
 
 CREATE TABLE dentist (
@@ -23,11 +24,11 @@ CREATE TABLE dentist (
 
 INSERT INTO demographics (first_name, last_name, DOB, gender, address, phone, email)
 VALUES
-    ('John', 'Doe', '1990-05-15', 'Male', '123 Main St, Charlotte, NC', '704-555-1234', 'john.doe@example.com'),
-    ('Jane', 'Smith', '1985-08-22', 'Female', '456 Elm St, Charlotte, NC', '704-555-5678', 'jane.smith@example.com'),
-    ('Alice', 'Johnson', '1978-03-10', 'Female', '789 Oak St, Charlotte, NC', '704-555-9101', 'alice.johnson@example.com'),
-    ('Bob', 'Brown', '1995-11-30', 'Male', '321 Pine St, Charlotte, NC', '704-555-1122', 'bob.brown@example.com'),
-    ('Charlie', 'Davis', '1982-07-19', 'Male', '654 Maple St, Charlotte, NC', '704-555-3344', 'charlie.davis@example.com');
+    ('John', 'Doe', '1990-05-15', 'Male', '123 Main St, Charlotte, NC', '704-555-1234', 'john.doe@example.com', '123-4567-8910'),
+    ('Jane', 'Smith', '1985-08-22', 'Female', '456 Elm St, Charlotte, NC', '704-555-5678', 'jane.smith@example.com', '012-3456-7891'),
+    ('Alice', 'Johnson', '1978-03-10', 'Female', '789 Oak St, Charlotte, NC', '704-555-9101', 'alice.johnson@example.com', '234-5678-9101'),
+    ('Bob', 'Brown', '1995-11-30', 'Male', '321 Pine St, Charlotte, NC', '704-555-1122', 'bob.brown@example.com', '109-8765-4321'),
+    ('Charlie', 'Davis', '1982-07-19', 'Male', '654 Maple St, Charlotte, NC', '704-555-3344', 'charlie.davis@example.com', '332-7724-7902');
     
    
 INSERT INTO dentist (first_name, last_name, addressOfPractice, specialty, phone, email)
