009b179d99c353b77e3dbdee8a690b2a82dd76c9
mwalla55 <124012816+mwalla55@users.noreply.github.com>
2025-03-14 13:46:02 -0400

    fixed billingInfo to align with trigger

--- billingInfoAndInventoryCreator.sql
+++ billingInfoAndInventoryCreator.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS billingInfo (
     first_name varchar(255),
     last_name varchar(255),
     amount int,
-    paymentStatus varchar(255),
+    paymentStatus ENUM('paid', 'unpaid'),
     patient_id int,
     appointment_ID int,
     FOREIGN KEY (patient_id) REFERENCES demographics(patient_id),
@@ -16,4 +16,9 @@ CREATE TABLE IF NOT EXISTS inventory (
 	item_id int AUTO_INCREMENT PRIMARY KEY,
     itemName varchar(255),
     quantityAvailable int
-)
+);
+
+CREATE TABLE IF NOT EXISTS treatmentCost (
+	treatment varchar(255),
+    cost int
+);
