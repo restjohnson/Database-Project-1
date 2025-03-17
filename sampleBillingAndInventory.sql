ed73a98a5efcab614cc3f00ef69878eef9525b8b
Rest Johnson <149099469+restjohnson@users.noreply.github.com>
2025-03-17 18:37:07 -0400

    updated trigger condition for only completed appointments

--- sampleBillingAndInventory.sql
+++ sampleBillingAndInventory.sql
@@ -8,17 +8,19 @@ BEGIN
 	DECLARE treatment_type VARCHAR(255); 
 	DECLARE treatment_cost DECIMAL(10,2); 
 	-- Get the treatment from patientRecord 
-	SELECT treatment_proposed INTO treatment_type FROM patientRecord WHERE record_id = NEW.record_id LIMIT 1; 
-	-- Get the cost from treatmentCost table 
-	SELECT cost INTO treatment_cost FROM treatmentCost WHERE treatment = treatment_type LIMIT 1;
-	-- If no cost found, default to 100.00 
-	IF treatment_cost IS NULL THEN SET treatment_cost = 100.00; 
-	END IF; 
-	-- Insert the billing record  
-	INSERT INTO billingInfo (first_name, last_name, appointment_id, patient_id, amount, paymentStatus) VALUES 
-	((SELECT first_name FROM demographics WHERE demographics.patient_id = NEW.patient_id LIMIT 1),
-	(SELECT last_name FROM demographics WHERE demographics.patient_id = NEW.patient_id LIMIT 1),
-	NEW.appointment_id, NEW.patient_id, treatment_cost, 'unpaid');  
+	IF NEW.appointment_status = 'Completed' AND OLD.appointment_status != 'Completed' THEN
+		SELECT treatment_proposed INTO treatment_type FROM patientRecord WHERE record_id = NEW.record_id LIMIT 1; 
+		-- Get the cost from treatmentCost table 
+		SELECT cost INTO treatment_cost FROM treatmentCost WHERE treatment = treatment_type LIMIT 1;
+		-- If no cost found, default to 100.00 
+		IF treatment_cost IS NULL THEN SET treatment_cost = 100.00; 
+		END IF; 
+		-- Insert the billing record  
+		INSERT INTO billingInfo (first_name, last_name, appointment_id, patient_id, amount, paymentStatus) VALUES 
+		((SELECT first_name FROM demographics WHERE demographics.patient_id = NEW.patient_id LIMIT 1),
+		(SELECT last_name FROM demographics WHERE demographics.patient_id = NEW.patient_id LIMIT 1),
+		NEW.appointment_id, NEW.patient_id, treatment_cost, 'unpaid');  
+	END IF;
 END; 
 // DELIMITER ;
 
